e9d940dd3f538d6c7cf98a54f9426e082d6a06c2
Jonathan Logan <jonathan.logan@aurora.dev>
2022-07-24 06:25:07 +0200

    cleanup

--- cmd/cfgtar/main.go
+++ cmd/cfgtar/main.go
@@ -12,30 +12,6 @@ import (
 
 // cat template.tar | cfgtar config | tar -x -C /
 
-//func main() {
-//	if len(os.Args) != 2 {
-//		_, _ = fmt.Fprintf(os.Stderr, "%s: Requires config file argument\n", os.Args[0])
-//		os.Exit(1)
-//	}
-//	f, err := os.Open(os.Args[1])
-//	if err != nil {
-//		_, _ = fmt.Fprintf(os.Stderr, "Cannot open config file (%s): %s\n", os.Args[1], err)
-//		os.Exit(2)
-//	}
-//	defer func() { _ = f.Close() }()
-//	config, err := lineconfig.ParseConfig(f)
-//	if err != nil {
-//		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
-//		os.Exit(3)
-//	}
-//	if err := tarpipe.TarPipe(os.Stdin, os.Stdout, config); err != nil {
-//		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
-//		os.Exit(4)
-//	}
-//	_ = os.Stdout.Sync()
-//	_ = os.Stdout.Close()
-//}
-
 func parseJsonFile(filename string) (interface{}, error) {
 	var ret interface{}
 	d, err := ioutil.ReadFile(filename)
